59b202b78b93bb21e14979d10f6ce4a35639884f
Bhanu <bhanukrsingh@gmail.com>
2018-06-11 20:23:59 +0530

    All the unecessary files are deleted and new course files are setup

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,16 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-import { ServerComponent } from './server/server.component';
-import { ServersComponent } from './servers/servers.component';
+
 import { FormsModule } from '@angular/forms';
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    ServerComponent,
-    ServersComponent
+    
   ],
   imports: [
     BrowserModule,

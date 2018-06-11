54f0498d2e6c089ee8e8b325cb64832259e9e520
Bhanu <bhanukrsingh@gmail.com>
2018-06-11 21:00:30 +0530

    Created basic components for the receipe app

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,24 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppComponent } from './app.component';
-
 import { FormsModule } from '@angular/forms';
+import { HeaderComponent } from './header/header.component';
+import { RecipesComponent } from './recipes/recipes.component';
+import { RecipeDetailComponent } from './recipes/recipe-detail/recipe-detail.component';
+import { ShoppingListComponent } from './shopping-list/shopping-list.component';
+import { ShoppingEditComponent } from './shopping-list/shopping-edit/shopping-edit.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
+    HeaderComponent,
+    RecipesComponent,
+
+    RecipeDetailComponent,
+
+    ShoppingListComponent,
+    ShoppingEditComponent
     
   ],
   imports: [
